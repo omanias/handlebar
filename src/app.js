1af9bb1f5d6e0c6132983f62bafe6991bbd2c5eb
Omar Jesús Maniás <ojmanias1985@gmail.com>
2024-04-18 20:57:40 -0300

    cambios en handlebars

--- src/app.js
+++ src/app.js
@@ -14,14 +14,43 @@ app.set('views', __dirname + '/views')
 app.set('view engine', 'handlebars')
 app.use(express.static(__dirname + '/public'))
 
+let food = [
+    { name: "Pizza", price: 100 },
+    { name: "Hamburguesa", price: 150 },
+    { name: "Papas rustica", price: 200 },
+    { name: "Empanadas", price: 250 },
+    { name: "Ensalada", price: 300 }
+]
 
 app.get('/', (req, res) => {
-    let user = {
-        nombre: "Coder",
-        edad: 25
+    let testUser = {
+        name: "Coder",
+        lastName: "House",
+        role: "user"
     }
+    res.render('index', {
+        user: testUser,
+        //css
+        isAdmin: testUser.role === "admin",
+        food
+    })
+})
 
-    res.render('index', user)
+let users = [];
+
+app.post('/user', (req, res) => {
+    const { name, email, password } = req.body;
+    const newUser = { name, email, password };
+    users.push(newUser);
+    res.send('Usuario registrado correctamente.');
+});
+
+app.get('/register', (req, res) => {
+    res.render('register');
+});
+
+app.get('/users', (req, res) => {
+    res.send(users);
 })
 
 app.listen(PORT, () => {
